10518c3eccfb836ff63e7efd52aaa39fb7ee124c
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-13 00:28:54 +0800

    move contact modal to below

--- client/src/components/ContactModal.tsx
+++ client/src/components/ContactModal.tsx
@@ -30,20 +30,27 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
   }, [isOpen, onClose]);
   if (!isOpen) return null;
   return (
-    <div className="primary-text fixed inset-0 flex w-full flex-col items-center justify-center bg-black/40 px-2">
+    <div className="primary-text fixed inset-0 flex w-full flex-col items-center justify-end bg-black/40 px-2">
       <div
         ref={modalRef}
-        className="relative w-full max-w-md rounded-lg bg-neutral-50 px-6 py-3 shadow-lg"
+        className="relative mb-20 w-full max-w-md rounded-lg bg-neutral-50 p-6 shadow-lg"
       >
         <p>Bagi sebarang pertanyaan, sila hubungi</p>
         <div className="my-6 flex w-full flex-col items-center gap-4 text-lg font-semibold">
           <div className="flex w-full items-center">
             <p className="w-3/5 text-start">Mohd Zin</p>
             <div className="flex w-2/5 justify-end gap-6">
-              <a href="tel:+60148357418" className="">
+              <a
+                href="tel:+60148357418"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <Phone />
               </a>
-              <a href="https://wa.me/60148357418" target="blank" className="">
+              <a
+                href="https://wa.me/60148357418"
+                target="blank"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <MessageCircle />
               </a>
             </div>
@@ -51,10 +58,17 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
           <div className="flex w-full items-center">
             <p className="w-3/5 text-start">Kak Na</p>
             <div className="flex w-2/5 justify-end gap-6">
-              <a href="tel:+601110995401" className="">
+              <a
+                href="tel:+601110995401"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <Phone />
               </a>
-              <a href="https://wa.me/601110995401" target="blank" className="">
+              <a
+                href="https://wa.me/601110995401"
+                target="blank"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <MessageCircle />
               </a>
             </div>
@@ -62,10 +76,17 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
           <div className="flex w-full items-center">
             <p className="w-3/5 text-start">Aidil</p>
             <div className="flex w-2/5 justify-end gap-6">
-              <a href="tel:+601117821968" className="">
+              <a
+                href="tel:+601117821968"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <Phone />
               </a>
-              <a href="https://wa.me/601117821968" target="blank" className="">
+              <a
+                href="https://wa.me/601117821968"
+                target="blank"
+                className="transition-colors duration-300 hover:text-[#d17b7b]"
+              >
                 <MessageCircle />
               </a>
             </div>
@@ -73,7 +94,7 @@ const ContactModal: React.FC<ContactModalProps> = ({ isOpen, onClose }) => {
         </div>
         <button
           onClick={onClose}
-          className="absolute top-0 right-0 m-2 cursor-pointer text-3xl"
+          className="absolute top-0 right-0 m-2 cursor-pointer text-3xl transition-colors duration-300 hover:text-[#d17b7b]"
         >
           <IoClose />
         </button>
