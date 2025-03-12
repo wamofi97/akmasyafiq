88351646f3b3269050a3345d84730ff69fc41ad3
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-12 18:49:09 +0800

    remove force ipv4

--- server/db.js
+++ server/db.js
@@ -4,7 +4,6 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Ensure SSL is handled correctly
-    host: "db.rwgdvcentksdtbumntov.supabase.co",
   },
 });
 
