4a2ca9b970c70f9e87b19b7b792f8b13c390e321
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-13 00:31:14 +0800

    add background

--- client/src/routes/WelcomePage.tsx
+++ client/src/routes/WelcomePage.tsx
@@ -20,49 +20,48 @@ const WelcomePage = ({
   };
 
   return (
-    <div className="relative mx-auto flex h-screen max-w-[700px] flex-col items-center justify-center overflow-hidden bg-white px-4 text-center">
-      <div className="relative h-full w-full bg-[url('/background.png')] bg-cover bg-center">
-        <div className="absolute inset-0 mx-auto flex max-w-[500px] flex-col items-center justify-center text-center">
-          <p className="cinzel accent-color text-xl font-bold tracking-[0.3em] uppercase">
-            Walimatul Urus
-          </p>
-          <div className="my-8 flex flex-col items-center">
-            <img src="/logo.png" alt="logo" className="my-4 w-32" />
-            <h1 className="names primary-color text-5xl font-medium tracking-wider">
-              Akma & Syafiq
-            </h1>
-          </div>
-          <div className="primary-text flex items-center gap-6 text-xl uppercase">
-            <div className="space-y-2">
-              <hr />
-              <p>Sabtu</p>
-              <hr />
-            </div>
-            <div>
-              <p>MEI</p>
-              <p className="text-4xl">03</p>
-              <p>2025</p>
-            </div>
-            <div className="space-y-2">
-              <hr />
-              <p>12.00 PM</p>
-              <hr />
-            </div>
-          </div>
-          <p className="crimson my-6 text-lg text-neutral-500 italic">
-            “Dan segala sesuatu kami ciptakan berpasangan supaya kamu mengingati
-            (kebesaran Allah).” [51:49]
-          </p>
-          <button
-            onClick={closeModal}
-            className="primary-text cursor-pointer rounded-lg bg-[#A75F5F] px-6 py-3 text-lg font-medium tracking-widest"
-          >
-            <p className="text-white">Open</p>
-          </button>
-          <p className="accent-color primary-text my-4">
-            #theperfectFiqforAkma
-          </p>
+    <div className="relative mx-auto flex h-screen max-w-[700px] flex-col items-center justify-center overflow-hidden bg-white text-center">
+      <div className="absolute hidden h-full w-full bg-[url('/background.webp')] bg-cover bg-center sm:block"></div>
+      <div className="absolute h-full w-3/4 -translate-x-28 bg-[url('/background.webp')] bg-cover bg-left sm:hidden"></div>
+      <div className="absolute h-full w-3/4 -translate-y-5 translate-x-28 bg-[url('/background.webp')] bg-cover bg-right sm:hidden"></div>
+      <div className="absolute inset-0 mx-auto flex max-w-[500px] flex-col items-center justify-center px-4 text-center">
+        <p className="cinzel accent-color text-xl font-bold tracking-[0.3em] uppercase">
+          Walimatul Urus
+        </p>
+        <div className="my-8 flex flex-col items-center">
+          <img src="/logo.png" alt="logo" className="my-4 w-32" />
+          <h1 className="names primary-color text-5xl font-medium tracking-wider">
+            Akma & Syafiq
+          </h1>
         </div>
+        <div className="primary-text flex items-center gap-6 text-xl uppercase">
+          <div className="space-y-2">
+            <hr />
+            <p>Sabtu</p>
+            <hr />
+          </div>
+          <div>
+            <p>MEI</p>
+            <p className="text-4xl">03</p>
+            <p>2025</p>
+          </div>
+          <div className="space-y-2">
+            <hr />
+            <p>12.00 PM</p>
+            <hr />
+          </div>
+        </div>
+        <p className="crimson my-6 text-lg text-neutral-500 italic">
+          “Dan segala sesuatu kami ciptakan berpasangan supaya kamu mengingati
+          (kebesaran Allah).” [51:49]
+        </p>
+        <button
+          onClick={closeModal}
+          className="primary-text cursor-pointer rounded-lg bg-[#A75F5F] px-6 py-3 text-lg font-medium tracking-widest transition-colors duration-300 hover:bg-[#b86969]"
+        >
+          <p className="text-white">Open</p>
+        </button>
+        <p className="accent-color primary-text my-4">#theperfectFiqforAkma</p>
       </div>
 
       {/* Curtain Effect */}
