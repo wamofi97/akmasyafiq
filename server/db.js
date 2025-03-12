6797ed05a6e597cc2a667b2ac3659e2fc71bce81
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-12 17:15:54 +0800

    test force ipv4

--- server/db.js
+++ server/db.js
@@ -4,6 +4,7 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false, // Ensure SSL is handled correctly
+    host: "db.rwgdvcentksdtbumntov.supabase.co",
   },
 });
 
