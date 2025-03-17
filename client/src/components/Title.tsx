70f5c37e15ba3c564417b9219516f1d8515cc991
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-17 21:33:24 +0800

    add tracking wider

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,7 +1,7 @@
 const Title = ({ title }: { title: string }) => {
   return (
     <div>
-      <h1 className="bilbo accent-color py-4 text-4xl font-semibold underline decoration-2 underline-offset-8 sm:text-5xl">
+      <h1 className="bilbo accent-color py-4 text-3xl font-semibold tracking-wider underline decoration-2 underline-offset-8 sm:text-4xl">
         {title}
       </h1>
     </div>
