8256c2dc94fbfe04d49997e37316f397c31fc885
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-17 23:00:53 +0800

    adjust size for mobile

--- client/src/components/Guestbook.tsx
+++ client/src/components/Guestbook.tsx
@@ -77,7 +77,7 @@ const Guestbook = () => {
 
       <button
         onClick={openGuestbookModal}
-        className="flex cursor-pointer items-center gap-2 rounded-lg bg-[#A75F5F] px-4 py-2 text-lg font-medium text-white transition-colors duration-300 hover:bg-[#b86969]"
+        className="flex cursor-pointer items-center gap-2 rounded-lg bg-[#A75F5F] px-4 py-2 font-medium text-white transition-colors duration-300 hover:bg-[#b86969] sm:text-lg"
       >
         Hantar Ucapan
       </button>
