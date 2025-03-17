8b29003b52be1e3b197f0c12e197e4bc837d0918
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-18 00:16:55 +0800

    add wishes2 for syafiqakma guestbook2

--- server/routes/wedding.js
+++ server/routes/wedding.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const pool = require("../db");
 
-// create wishes
+// create wishes for akmasyafiq
 router.post("/wishes", async (req, res) => {
   try {
     const { name, message } = req.body;
@@ -17,10 +17,40 @@ router.post("/wishes", async (req, res) => {
   }
 });
 
-// get wishes
+// get wishes for akma syafiq
 router.get("/wishes", async (req, res) => {
   try {
     const result = await pool.query("SELECT * from guestbook ORDER BY id DESC");
+    const wishesData = result.rows;
+    res.status(200).json(wishesData);
+  } catch (error) {
+    console.error("Error getting wishes", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// create wishes for syafiqakma
+router.post("/wishes2", async (req, res) => {
+  try {
+    const { name, message } = req.body;
+    const result = await pool.query(
+      "INSERT INTO guestbook2 ( name, message) VALUES ($1, $2) RETURNING *",
+      [name, message]
+    );
+    const wishesData = result.rows[0];
+    res.status(200).json(wishesData);
+  } catch (error) {
+    console.error("Error creating wishes", error);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// get wishes for akma syafiq
+router.get("/wishes2", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT * from guestbook2 ORDER BY id DESC"
+    );
     const wishesData = result.rows;
     res.status(200).json(wishesData);
   } catch (error) {
