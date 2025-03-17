6155a9ff3ea4776b0a3f82f23445a36cec43ece3
Wan Mohd Firdaus <wamofi97@gmail.com>
2025-03-17 21:27:29 +0800

    change fonts style

--- client/src/components/Title.tsx
+++ client/src/components/Title.tsx
@@ -1,7 +1,9 @@
 const Title = ({ title }: { title: string }) => {
   return (
     <div>
-      <h1 className="accent-color cinzel py-4 text-3xl font-bold">{title}</h1>
+      <h1 className="bilbo accent-color py-4 text-4xl font-semibold underline decoration-2 underline-offset-8 sm:text-5xl">
+        {title}
+      </h1>
     </div>
   );
 };
